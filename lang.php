45abb7746fb8cb57086d1928bded26e73e85ee0f
sokolik2135 <39133910+sokolik2135@users.noreply.github.com>
2018-12-29 18:31:59 +0100

    Add files via upload

--- lang.php
+++ lang.php
@@ -3,7 +3,7 @@
     $list = array();
     $i = 0;
     while (false !== ($entry = $d->read())) {
-        if (is_dir($entry) && $entry != '.' && $entry != '..' && $entry != 'css' && $entry != 'dev' && $entry != 'fonts' && $entry != 'icons' && $entry != 'js' && $entry != 'res') {
+        if (is_dir($entry) && $entry != '.' && $entry != '..' && $entry != 'css' && $entry != 'dev' && $entry != 'fonts' && $entry != 'icons' && $entry != 'js' && $entry != 'res' && $entry != 'vendor' && $entry != '.composer' && $entry != '.heroku' && $entry != '.profile.d') {
             $list[$i] = $entry;
             $i = $i + 1;
         }
