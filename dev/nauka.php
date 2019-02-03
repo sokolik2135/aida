38c96ab8f07c6a07f9712444c743e6bc02f9ebe4
sokolik2135 <39133910+sokolik2135@users.noreply.github.com>
2019-02-03 10:49:07 +0100

    Add files via upload

--- dev/nauka.php
+++ dev/nauka.php
@@ -2,4 +2,6 @@
     $data = file_get_contents('./nauka.txt');
     $retrieved = "\n'".$_GET['q']."': '".$_GET['a']."',";
     file_put_contents('./nauka.txt',$data.$retrieved);
+    $msg = "Niezrozumiała informacja: ".$_GET['q']."\nSugerowana odpowiedź: ".$_GET['a'];
+    mail('sokolowski776@gmail.com','Aida - sugestia odpowiedzi',$msg);
 ?>
